01c846898a6be6fd871f5e733d9897421dec652a
calmenelias <39606@alunos.isel.ipl.pt>
2016-01-18 12:13:49 +0000

    Script construction updated

--- NGSPipesEngine/src/ngspipesengine/configurator/scripts/Script.java
+++ NGSPipesEngine/src/ngspipesengine/configurator/scripts/Script.java
@@ -8,11 +8,12 @@ import ngspipesengine.utils.IO;
 
 public class Script {
 
-	static final String BEGIN_SCRIPT = "#!/bin/bash";
+	static final String BEGIN_SCRIPT = "#!/bin/bash\n";
 	static final String BEGIN_VERIFY_EXIST_COMMAND = 	"if which docker ; then \n echo exist\n	else\n\t";
 	static final String END_IF_COMMAND = "fi";
 	static final String SHARED_FOLDER_COMMAND = "if [ ! -f %1$s ]; then\n\tmount -t vboxsf %2$s %1$s";
 	static final String POWER_OFF_MACHINE = "\nsudo /sbin/shutdown -h now";
+	static final String UPDATE_COMMAND = " apt-get update";
 
 	private static final String ROOT_COMMAND = "sudo";
 	
@@ -23,6 +24,7 @@ public class Script {
 
 	public static void createExecute(Map<String, List<String>> commands, String runnerCommand) throws EngineException {
 		StringBuilder script = new StringBuilder(BEGIN_SCRIPT);
+		script.append(ROOT_COMMAND).append(UPDATE_COMMAND);
 		getScriptContent(commands, script);
 		script	.append("\n").append(ROOT_COMMAND).append(" ")
 				.append(runnerCommand)
@@ -50,8 +52,12 @@ public class Script {
 	private static StringBuilder getSetups(List<String> setups) {
 		StringBuilder commands = new StringBuilder();
 
-		for (String c : setups)
-			commands.append(ROOT_COMMAND).append(" ").append(c).append("\n");
+		for (String c : setups) {
+			if(c.contains(ROOT_COMMAND))
+				commands.append(ROOT_COMMAND).append(" ");
+			
+			commands.append(c).append("\n");
+		}
 		return commands;
 	}
 }
