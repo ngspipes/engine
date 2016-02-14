c733f7ddc26ba1fce1808853bda8ae1c1600e380
Alexandre P Francisco <aplf@tecnico.pt>
2016-02-14 00:41:28 +0000

    Conciliate with previous code.

--- src/main/java/ngspipesengine/console/NGSPipesEngineConsole.java
+++ src/main/java/ngspipesengine/console/NGSPipesEngineConsole.java
@@ -1,3 +1,22 @@
+/*-
+ * Copyright (c) 2016, NGSPipes Team <ngspipes@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of NGSPipes <http://ngspipes.github.io/>.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package ngspipesengine.console;
 
 import java.io.BufferedReader;
@@ -98,6 +117,11 @@ public class NGSPipesEngineConsole {
 	private static void runEngine(CommandLine cmdLine) throws JSONException, EngineException, IOException {
 		VMProperties vmProperties = getVMProperties(cmdLine);
 		VMEngine vm = new VMEngine(vmProperties);
+
+		System.out.println(
+				String.format("Starting executor with %d CPUs and %d GBytes",
+						vmProperties.getProcessorsNumber(),
+						vmProperties.getMemory()/1024));
 
 		vm.start();
 		initServer(vmProperties);
