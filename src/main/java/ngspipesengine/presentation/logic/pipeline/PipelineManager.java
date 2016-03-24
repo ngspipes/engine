e2f95cedd7e3ac87750cb1ad70f98a3cac9b7d54
BrunoMNDantas <bmndantas@gmail.com>
2016-03-24 15:40:06 +0000

    EngineManager filtering invalid pipelines

--- src/main/java/ngspipesengine/presentation/logic/pipeline/PipelineManager.java
+++ src/main/java/ngspipesengine/presentation/logic/pipeline/PipelineManager.java
@@ -20,10 +20,10 @@
 package ngspipesengine.presentation.logic.pipeline;
 
 import ngspipesengine.core.exceptions.EngineException;
-import ngspipesengine.presentation.ui.utils.Uris;
+import ngspipesengine.core.utils.IO;
 import ngspipesengine.presentation.exceptions.EnginePresentationException;
 import ngspipesengine.presentation.logic.engine.EngineManager;
-import ngspipesengine.core.utils.IO;
+import ngspipesengine.presentation.ui.utils.Uris;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,7 +32,7 @@ import java.util.LinkedList;
 
 public class PipelineManager {
 
-	private static final String PREVIOUS_PIPELINES = Uris.PREVIOUS_PIPELINES + Uris.SEP + "pipelines.json";
+	private static final String PREVIOUS_PIPELINES_PATH = Uris.PREVIOUS_PIPELINES + Uris.SEP + "previous_pipelines.json";
 	private static final Object lock = new Object();
 	private static Collection<Pipeline> PIPELINES;
 
@@ -40,46 +40,76 @@ public class PipelineManager {
 	public static void load() throws EnginePresentationException {
 		synchronized (lock){
 			try {
-				String serializedPipelines = null;
-
-				File previousPipelines = new File(PREVIOUS_PIPELINES);
-				if(previousPipelines.exists())
-					serializedPipelines = IO.readFile(PREVIOUS_PIPELINES);
-				else{
-					new File(Uris.PREVIOUS_PIPELINES).mkdirs();
-					previousPipelines.createNewFile();
-				}
-
+				String serializedPipelines = getSerializedPipelines();
 
 				PIPELINES = PipelineSerializer.deserialize(serializedPipelines);
-				validatePipelines(PIPELINES);
+
+				cleanPipelines(PIPELINES);
 			} catch (EngineException | IOException ex) {
 				throw new EnginePresentationException("Error loading pipelines!", ex);
+			} finally{
+				if(PIPELINES == null)
+					PIPELINES = new LinkedList<>();
 			}
 		}
 	}
 
-	private static void validatePipelines(Collection<Pipeline> pipelines) throws EnginePresentationException {
+	private static String getSerializedPipelines() throws EngineException, IOException {
+		File previousPipelines = new File(PREVIOUS_PIPELINES_PATH);
+
+		if(previousPipelines.exists())
+			return IO.readFile(PREVIOUS_PIPELINES_PATH);
+
+		new File(Uris.PREVIOUS_PIPELINES).mkdirs();
+		previousPipelines.createNewFile();
+		return null;
+	}
+
+	private static void cleanPipelines(Collection<Pipeline> pipelines) throws EnginePresentationException {
+		validateEngineNames(pipelines);
+
+		if(filterNonexistentDirectoriesOrFiles(pipelines))
+			throw new EnginePresentationException("Some previous pipelines were discarded because pipeline, output or input directories doesn't exist.");
+	}
+
+	private static void validateEngineNames(Collection<Pipeline> pipelines) throws EnginePresentationException {
 		String defaultEngineName = EngineManager.getDefaultEngineName();
 		Collection<String> enginesNames = EngineManager.getEnginesNames();
 		enginesNames.add(defaultEngineName);
 
-		pipelines.forEach((pipeline)->{
+		pipelines.forEach((pipeline) -> {
 			if(!enginesNames.contains(pipeline.getEngineName()))
 				pipeline.setEngineName(EngineManager.getDefaultEngineName());
 		});
 	}
-	
+
+	private static boolean filterNonexistentDirectoriesOrFiles(Collection<Pipeline> pipelines) {
+		boolean hasPipelinesWithNonexistentDirs = false;
+
+		for(Pipeline pipeline : new LinkedList<>(pipelines)){
+			if(!pipeline.getPipeline().exists() || !pipeline.getInputDir().exists() || !pipeline.getOutputDir().exists()){
+				pipelines.remove(pipeline);
+				hasPipelinesWithNonexistentDirs = true;
+			}
+		}
+
+		return hasPipelinesWithNonexistentDirs;
+	}
+
+
+
 	public static void save() throws EnginePresentationException {
 		synchronized (lock){
 			try{
 				String serializedPipelines = PipelineSerializer.serialize(PIPELINES);
-				IO.writeToFile(PREVIOUS_PIPELINES, serializedPipelines, false);
+				IO.writeToFile(PREVIOUS_PIPELINES_PATH, serializedPipelines, false);
 			} catch(EngineException ex) {
 				throw new EnginePresentationException("Error saving pipelines!", ex);
 			}
 		}
 	}
+
+
 
 	public static Collection<Pipeline> getAll(){
 		synchronized (lock){
@@ -87,13 +117,17 @@ public class PipelineManager {
 		}
 	}
 
+
+
 	public static void add(Pipeline pipeline){
 		synchronized (lock) {
 			if(!PIPELINES.contains(pipeline))
 				PIPELINES.add(pipeline);
 		}
 	}
-	
+
+
+
 	public static void remove(Pipeline pipeline){
 		synchronized (lock){
 			if(PIPELINES.contains(pipeline))
